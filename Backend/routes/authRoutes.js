0a838fa96d1689fdd5165eac7a09ee4a043bd90c
yaz-oss <ishimweyaziid749@gmail.com>
2026-05-07 11:22:41 +0200

    fix backend for user to create account

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,3 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/User");
+
+
+
+// REGISTER
+
 router.post(
   "/register",
 
@@ -36,6 +50,7 @@ router.post(
       let role = "user";
 
       if (totalUsers === 0) {
+
         role = "admin";
       }
 
@@ -61,3 +76,79 @@ router.post(
     }
   }
 );
+
+
+
+// LOGIN
+
+router.post(
+  "/login",
+
+  async (req, res) => {
+
+    try {
+
+      const {
+        email,
+        password,
+      } = req.body;
+
+      const user =
+        await User.findOne({
+          where: { email },
+        });
+
+      if (!user) {
+
+        return res.status(400).json({
+          message:
+            "User not found",
+        });
+      }
+
+      const validPassword =
+        await bcrypt.compare(
+          password,
+          user.password
+        );
+
+      if (!validPassword) {
+
+        return res.status(400).json({
+          message:
+            "Wrong password",
+        });
+      }
+
+      const token =
+        jwt.sign(
+          {
+            id: user.id,
+            role: user.role,
+          },
+
+          "secretkey"
+        );
+
+      res.json({
+        token,
+
+        user: {
+          id: user.id,
+          username:
+            user.username,
+          email:
+            user.email,
+          role:
+            user.role,
+        },
+      });
+
+    } catch (error) {
+
+      res.status(500).json(error);
+    }
+  }
+);
+
+module.exports = router;
